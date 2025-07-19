058c25d66b8c38abca7be750b49c91da1a22ac37
BHaafkes <barthaafkes@hotmail.com>
2025-07-19 12:41:23 +0200

    Update main.py

--- .github/main.py
+++ .github/main.py
@@ -9,6 +9,5 @@ def hello_world():
     return "Hello, World! The test service is running correctly."
 
 if __name__ == "__main__":
-    # This block is for local testing, not used by Gunicorn/Cloud Run.
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
